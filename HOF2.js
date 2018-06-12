b88a2027588d2b5febba9ce527ddd82ea57cf414
Will Holcombe <william.holcombe@uconn.edu>
2018-06-12 16:02:09 -0400

    added second function

--- HOF2.js
+++ HOF2.js
@@ -28,10 +28,29 @@ const repeat2 = function (fn, n){
 repeat2(hello2,4);
 */
 /*=============================================*/
+// DO NOT EDIT BETWEEN THESE LINES ----->
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
-const filter = function (arr, fn){
-  
+const filteredNames = filter(myNames, function(name) {
+    // This is a "predicate function" - it's a function that only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+
+function filter(arr, fn){
+    let newArray = [];
+    for(let i = 0; i<arr.length; i++){
+      if(fn(arr[i])=== true){
+          newArray.push(arr[i]);
+      }
+    }
+    return newArray;
 }
-
+filter(myNames, filteredNames);
 
 
